1820e9df61736139e809a13fdef842fd9b2fff4d
Matheus <matheuspsilva29@gmail.com>
2022-07-14 19:54:55 -0300

    Adição endpoint User update com tratamento de exceção de e-mail

--- src/main/java/com/matheussilvadev/resource/UserResource.java
+++ src/main/java/com/matheussilvadev/resource/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +53,16 @@ public class UserResource {
 		//Retorna URI de acesso ao novo objeto
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+		
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO userDTO){
+		//Garantir que o usuário tem o id recebido na url
+		userDTO.setId(id);
+		User newUser = service.update(userDTO);
+		
+		return ResponseEntity.ok().body(mapper.map(newUser, UserDTO.class));
 		
 	}
 
--- src/main/java/com/matheussilvadev/services/impl/UserServiceImpl.java
+++ src/main/java/com/matheussilvadev/services/impl/UserServiceImpl.java
@@ -43,9 +43,15 @@ public class UserServiceImpl implements UserService{
 	
 	private void findByEmail(UserDTO obj) {
 		Optional<User> user = userRepository.findByEmail(obj.getEmail());
-		if(user.isPresent()) {
+		if(user.isPresent() && !user.get().getId().equals(obj.getId())) {
 			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema");
 		}
+	}
+
+	@Override
+	public User update(UserDTO userDTO) {
+		findByEmail(userDTO);
+		return userRepository.save(modelMapper.map(userDTO, User.class));
 	}
 
 }
