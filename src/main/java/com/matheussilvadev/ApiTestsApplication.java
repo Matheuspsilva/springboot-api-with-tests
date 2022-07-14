674e7f26c3348f1929a3ca6c0fc14898f586872f
Matheus <matheuspsilva29@gmail.com>
2022-07-13 21:02:33 -0300

    Adição de User model com lombok

--- src/main/java/com/matheussilvadev/ApiTestsApplication.java
+++ src/main/java/com/matheussilvadev/ApiTestsApplication.java
@@ -3,11 +3,15 @@ package com.matheussilvadev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.matheussilvadev.domain.User;
+
 @SpringBootApplication
 public class ApiTestsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTestsApplication.class, args);
+		
+		User user = new User(1, "Matheus", "matheus@email.com", "1234");
 	}
 
 }
